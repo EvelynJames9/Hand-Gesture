090fe3169d57324b6bc10659d76280712e01533b
EvelynJames9 <98927398+EvelynJames9@users.noreply.github.com>
2022-07-11 10:36:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ Webcam.attach( '#camera' );
 
 function take_snapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("restult").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';   
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';   
     });
 }
 
@@ -23,8 +23,38 @@ function modelLoaded() {
     console.log('Model Loaded!');
 }
 
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
 function speak(){
     var synth = window.speechSynthesis;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    speak_data = toSpeak;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
+}
+function gotResult(error, results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        gesture = results[0].label;
+        toSpeak="";
+        speak();
+        if(gesture == "amazing"){
+            toSpeak = "This is looking amazing";
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#128076;";
+        }
+        else if(gesture == "best"){
+            toSpeak = "All the best";
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;";
+        }
+        else if(gesture == "victory"){
+            toSpeak = "That was a marvelous victory";
+            document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;";
+        }
+        speak();
+    }
 }
